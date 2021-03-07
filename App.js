5aa15e852853bbe785e03bb67fc3e446104727ba
Samar-100 <67325192+Samar-100@users.noreply.github.com>
2021-03-07 21:21:58 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import InstagramScreen from "./screens/in";
+import FacebookScreen from "./screens/fb";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <AppContainer />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const TabNavigator = createBottomTabNavigator({
+  Instagram: { screen: InstagramScreen },
+  Facebook: { screen: FacebookScreen },
 });
+
+const AppContainer = createAppContainer(TabNavigator);
---
+++ screens/fb.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+
+export default class FacebookScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Facebook Screen</Text>
+      </View>
+    );
+  }
+}
